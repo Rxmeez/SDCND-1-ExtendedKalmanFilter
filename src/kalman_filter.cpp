38670e2d0c5bd301682540b0da76c2a3a7f1e9b8
rxmeez <rameezkhan.1@hotmail.co.uk>
2017-11-05 23:00:59 +0000

    Complete TODO's from the code and running it on the simulator, currently working towards a lower RMSE

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -25,6 +25,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;  //x = Fx + u where u = 0
+  MatrixXd Ft = F_.transpose();
+  P_ = (F_ * P_ * Ft) + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +35,17 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd y = z - (H_ * x_);
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = (H_ * P_ * Ht) + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = (P_ * Ht * Si);
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - (K * H_)) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +53,40 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  //recover state parameters
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  // Equations for h_func below
+  float rho = sqrt(px * px + py * py);
+  float phi = atan2(py,px);
+  float rhodot;
+  //check division by zero
+  if(fabs(rho) < 0.0001)
+  {
+    rhodot = 0;
+  }else{
+    rhodot = (px*vx+py*vy)/rho;
+  }
+
+  //Feed in equations above
+  VectorXd H_func(3);
+  H_func << rho,
+            phi,
+            rhodot;
+
+  VectorXd y = z - H_func;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = (H_ * P_ * Ht) + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = (P_ * Ht * Si);
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - (K * H_) * P_);
+
 }
